7e41a71ef94276ea9541420d0473da0867b3c870
Lucio Chen <luciochen233@gmail.com>
2020-02-06 18:50:26 -0500

    updated new project

---
+++ repo/Class4/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Class4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Model _myModel;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _myModel = new Model();
+            this.DataContext = _myModel;
+        }
+
+        private void Bexit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Bcopy_Click(object sender, RoutedEventArgs e)
+        {
+            _myModel.CopyText();
+        }
+
+        private void TopBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            TextBox box = (TextBox)sender;
+            if(e.Key == Key.Enter)
+            {
+                box.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+                _myModel.CopyText();
+            }
+        }
+    }
+}
---
+++ repo/Class4/Model.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Class4
+{
+    class Model : INotifyPropertyChanged
+    {
+        public Model()
+        {
+            TopBoxText = " lucio ";
+            BotBoxText = " ";
+        }
+
+        public void CopyText()
+        {
+            BotBoxText = TopBoxText;
+        }
+
+        private string _topBoxText;
+        public string TopBoxText
+        {
+            get { return _topBoxText; }
+            set
+            {
+                _topBoxText = value;
+                OnPropertyChanged("TopBoxText");
+            }
+        }
+
+        private string _botBoxText;
+        public string BotBoxText
+        {
+            get { return _topBoxText; }
+            set
+            {
+                _botBoxText = value;
+                OnPropertyChanged("BotBoxText");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
