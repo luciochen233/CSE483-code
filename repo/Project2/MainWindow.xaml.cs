2ad3e11f8279612edb0c85a789050c54d2571719
Lucio Chen <luciochen233@gmail.com>
2020-02-06 21:10:28 -0500

    updated project2

--- repo/Project2/MainWindow.xaml.cs
+++ repo/Project2/MainWindow.xaml.cs
@@ -20,81 +20,31 @@ namespace Project2
     /// </summary>
     public partial class MainWindow : Window
     {
-        private string operation = "";
+        Model _myModel;
         public MainWindow()
         {
             InitializeComponent();
+            _myModel = new Model();
+            this.DataContext = _myModel;
         }
 
-        private void Bplus_Click(object sender, RoutedEventArgs e)
+        private void Operation_Button_Click(object sender, RoutedEventArgs e)
         {
-            Text_operation.Text = "Plus";
-            operation = "plus";
-        }
-
-        private void Bminus_Click(object sender, RoutedEventArgs e)
-        {
-            Text_operation.Text = "Minus";
-            operation = "minus";
-        }
-
-        private void Btimes_Click(object sender, RoutedEventArgs e)
-        {
-            Text_operation.Text = "Times";
-            operation = "times";
-        }
-
-        private void Bdivide_Click(object sender, RoutedEventArgs e)
-        {
-            Text_operation.Text = "Divide";
-            operation = "divide";
-        }
-
-        private void MainWindow1_Loaded(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private double GetResult(double x, double y, string op)
-        {
-            switch (op)
-            {
-                case "times": return x * y;
-                case "minus": return x - y;
-                case "divide": return (x / y);
-                case "plus": return x + y;
-            }
-            return 0.0;
+            System.Windows.Controls.Button bt = (System.Windows.Controls.Button)sender;
+            if (bt.Name.Contains("Bplus")) _myModel.Operation = "plus";
+            if (bt.Name.Contains("Bminus")) _myModel.Operation = "minus";
+            if (bt.Name.Contains("Btimes")) _myModel.Operation = "times";
+            if (bt.Name.Contains("Bdivide")) _myModel.Operation = "divide";
         }
 
         private void Bequal_Click(object sender, RoutedEventArgs e)
         {
-            double firstNum = 0;
-            double secondNum = 0;
-            try
-            {
-                firstNum = double.Parse(Input1.Text);
-                secondNum = double.Parse(Input2.Text);
-                double temp = GetResult(firstNum, secondNum, operation);
-                Result.Text = temp.ToString();
-                Text_error.Text = "";
-            }
-            catch(Exception ex)
-            {
-                Text_error.Text = ex.Message;
-                Text_error.Foreground = Brushes.Red;
-            }
-
+            _myModel.Calculate();
         }
 
         private void Bclear_Click(object sender, RoutedEventArgs e)
         {
-            Input1.Clear();
-            Input2.Clear();
-            Text_operation.Clear();
-            Result.Clear();
-            operation = "";
-            Text_error.Text = "All clear";
+            _myModel.Clear();
         }
 
         private void Bexit_Click(object sender, RoutedEventArgs e)
---
+++ repo/Project2/Model.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Project2
+{
+    class Model : INotifyPropertyChanged
+    {
+        public Model()
+        {
+            LeftNum = "Left";
+            RightNum = "Right";
+            Operation = "Operation";
+            Result = "Result";
+
+        }
+
+        public void Clear()
+        {
+            LeftNum = " ";
+            RightNum = " ";
+            Operation = " ";
+            Result = " ";
+            ErrMsg = "All clear";
+        }
+        private double GetResult(double x, double y, string op)
+        {
+            switch (op)
+            {
+                case "times": return x * y;
+                case "minus": return x - y;
+                case "divide": return (x / y);
+                case "plus": return x + y;
+            }
+            return 0.0;
+        }
+        public void Calculate()
+        {
+            double firstNum = 0;
+            double secondNum = 0;
+            try
+            {
+                firstNum = double.Parse(LeftNum);
+                secondNum = double.Parse(RightNum);
+                double temp = GetResult(firstNum, secondNum, Operation);
+                Result= temp.ToString();
+                ErrMsg = "";
+            }
+            catch (Exception ex)
+            {
+                ErrMsg = ex.Message;
+            }
+        }
+
+        private string _leftNum;
+        public string LeftNum
+        {
+            get { return _leftNum; }
+            set
+            {
+                _leftNum = value;
+                OnPropertyChanged("LeftNum");
+            }
+        }
+
+        private string _rightNum;
+        public string RightNum
+        {
+            get { return _rightNum; }
+            set
+            {
+                _rightNum = value;
+                OnPropertyChanged("RightNum");
+            }
+        }
+
+        private string _result;
+        public string Result
+        {
+            get { return _result; }
+            set
+            {
+                _result = value;
+                OnPropertyChanged("Result");
+            }
+        }
+
+        private string _errMsg;
+        public string ErrMsg
+        {
+            get { return _errMsg; }
+            set
+            {
+                _errMsg = value;
+                OnPropertyChanged("ErrMsg");
+            }
+        }
+
+        private string _operation;
+        public string Operation
+        {
+            get { return _operation; }
+            set
+            {
+                _operation = value;
+                OnPropertyChanged("Operation");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
